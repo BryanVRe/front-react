a4bdc466643e077c47bab0476d302315dc941847
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2023-03-28 10:06:42 -0600

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bryan Valerio Reyes        </p>
+          Bryan Valerio Reyes zs20006768 </p>
         <a
           className="App-link"
           href="https://reactjs.org"
