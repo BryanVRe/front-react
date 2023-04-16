bcd0cf32b2137326cfe5b9436b50bba463aa876c
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2023-04-16 16:42:30 -0600

    branch rick_morty

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bryan Valerio Reyes zs20006768 </p>
+          Rick and Morty App 
+
+<br></br>	  By Bryan Valerio Reyes PhD</p>
         <a
           className="App-link"
           href="https://reactjs.org"
